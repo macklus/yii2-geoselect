4885a284453bc0958820c04219eec4b6b37d9417
José Pedro Andrés <macklus@debianitas.net>
2016-02-24 11:46:45 +0100

    * Asset test

---
+++ assets/GeoSelectAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class GeoSelectAsset extends AssetBundle
+{
+    public $basePath = '@vendor/macklus/geoselect/themes/default';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
